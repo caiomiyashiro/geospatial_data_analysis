6474bb8447f5758a4443be721da3e94bd2f285c1
Caio Miyashiro <caiohenrique37@gmail.com>
2019-11-21 21:25:15 +0100

    finished docker compose v1

--- AMLD-2020/setup-osrm.sh
+++ AMLD-2020/setup-osrm.sh
@@ -1,10 +1,11 @@
 #!/env/sh
 
-if [ ! -f "new-york-latest.osm.pbf" ]; then
-  wget http://download.geofabrik.de/north-america/us/new-york-latest.osm.pbf
+if [ ! -f "router/new-york-latest.osm.pbf" ]; then
+  mkdir router
+  wget http://download.geofabrik.de/north-america/us/new-york-latest.osm.pbf -O router/new-york-latest.osm.pbf
 else
   echo "File exists!"
 fi
-docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/new-york-latest.osm.pbf
-docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-partition /data/new-york-latest.osm.pbf
-docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-customize /data/new-york-latest.osm.pbf
+docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/router/new-york-latest.osm.pbf
+docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-partition /data/router/new-york-latest.osm.pbf
+docker run -t -v "${PWD}:/data" osrm/osrm-backend osrm-customize /data/router/new-york-latest.osm.pbf
